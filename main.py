7bd65d1af0a9f56169e60a54e47e868af921c6cc
Adrian Popescu <adrianpopescupx@gmail.com>
2022-12-04 18:59:47 +0200

    Resizing Images

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import pygame
 import time
 import math
+from utils import scale_image
 
-GRASS = pygame.image.load("images/grass.jpg")
-TRACK = pygame.image.load("images/track.png")
+GRASS = scale_image(pygame.image.load("images/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("images/track.png"), 0.9)
 
 TRACK_BORDER = pygame.image.load("images/track-border.png")
 FINISH = pygame.image.load("images/finish.png")
 
-RED_CAR = pygame.image.load("images/red-car.png")
-GREEN_CAR = pygame.image.load("images/green-car.png")
+RED_CAR = scale_image(pygame.image.load("images/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("images/green-car.png"), 0.55)
 
 #Setting Up My Game Window
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
+
+FPS = 60
+
+def draw(win, images):
+    for img, pos in images:
+        win.blit(img, pos)
+
+run = True
+clock = pygame.time.Clock()
+images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+
+while run:
+    clock.tick(FPS) #Game limited to 60 Frames Per Second
+
+    draw(WIN, images)
+    pygame.display.update()
+        
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+pygame.quit()
