7c769b0b7e769eed5a1e0323713a6a2780dde689
Adrian Popescu <116150269+AdrianPopescuPx@users.noreply.github.com>
2022-12-05 09:59:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -19,6 +19,24 @@ pygame.display.set_caption("Racing Game!")
 
 FPS = 60
 
+class AbstractCar:
+    def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1 
+        
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+
+
 def draw(win, images):
     for img, pos in images:
         win.blit(img, pos)
