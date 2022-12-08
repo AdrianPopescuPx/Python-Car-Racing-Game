397767c6a4908f0ca53ee1902b2a5b67b892d5d4
Adrian Popescu <116150269+AdrianPopescuPx@users.noreply.github.com>
2022-12-08 22:10:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -101,8 +101,28 @@ class ComputerCar(AbstractCar):
         super().draw(win)
         # self.draw_points(win)
 
+    def calculate_angle(self):
+        target_x, target_y = self.path[self.current_point]
+        x_diff = target_x - self.x
+        y_diff = target_y - self.y
 
-        
+        if y_diff == 0:
+            desired_radian_angle = math.pi / 2
+        else:
+            desired_radian_angle = math.atan(x_diff / y_diff)
+
+        if target_y > self.y:
+            desired_radian_angle += math.pi
+
+        difference_in_angle = self.angle - math.degrees(desired_radian_angle)
+        if difference_in_angle >= 180:
+            difference_in_angle -= 360
+
+        if difference_in_angle > 0:
+            self.angle -= min(self.rotation_vel, abs(difference_in_angle))
+        else:
+            self.angle += min(self.rotation_vel, abs(difference_in_angle))
+
 
 def draw(win, images, player_car):
     for img, pos in images:
